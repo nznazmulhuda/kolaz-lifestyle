8dffea506f37bfb856bcc78c748ef8f34b5f7294
Nazmul Huda <nznazmulhuda@users.noreply.github.com>
2025-07-03 13:34:00 +0600

    project deploy on vercel and add vercel anylytics components✅

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "@/contexts/AppContext";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,8 @@ export default function RootLayout({
           {children}
           <Toaster />
         </AppProvider>
+
+        <Analytics />
       </body>
     </html>
   );
